ecec6763c6fe34c1fe09b92168773ab50d971c5d
PenZenMaster <george.penzenik@gmail.com>
2025-04-02 18:13:59 -0700

    Missed file in payload

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# main.py
+
+# Author: Skippy the Magnificent along with that dumb ape, George Penzenik
+# Version: 1.00
+# Date Modified: 16:21 04/02/2025
+# Comment:
+#  - Launches UI with Skippy splash screen, version header, and full functionality
+
 from ui import launch_ui
 
 if __name__ == "__main__":
