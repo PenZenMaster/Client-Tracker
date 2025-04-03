fb0a565c61559c917859a08d1f7507b6ea3d1a36
PenZenMaster <george.penzenik@gmail.com>
2025-04-03 14:34:28 -0700

    Updated imports for google-ads v26+ Removed deprecated v15 references

--- keyword_volume.py
+++ keyword_volume.py
@@ -1,34 +1,43 @@
 # Author: Skippy the Magnificent along with that dumb ape, George Penzenik
-# Version: 1.00
-# Date Modified: 23:42 04/03/2025
+# Version: 1.01
+# Date Modified: 18:26 04/03/2025
 # Comment:
-#  - Initial version to connect to Google Ads API and fetch keyword volume for a URL + seed keywords
+#  - Updated imports for google-ads v26+
+#  - Removed deprecated v15 references
 
 import sys
 from google.ads.googleads.client import GoogleAdsClient
-from google.ads.googleads.v15.enums.types.keyword_plan_network import KeywordPlanNetwork
-from google.ads.googleads.v15.services.types.keyword_plan_idea_service import GenerateKeywordIdeasRequest, KeywordAndUrlSeed
-from google.ads.googleads.v15.services.services.keyword_plan_idea_service import KeywordPlanIdeaServiceClient
-from google.ads.googleads.v15.resources.types import KeywordPlanHistoricalMetrics
+from google.ads.googleads.services.types import (
+    GenerateKeywordIdeasRequest,
+    KeywordAndUrlSeed,
+)
+from google.ads.googleads.enums.types import KeywordPlanNetwork
 
 
-def fetch_keyword_ideas(client, customer_id, page_url, seed_keywords, language_code="1000", geo_targets=["2840"]):
+def fetch_keyword_ideas(
+    client,
+    customer_id,
+    page_url,
+    seed_keywords,
+    language_code="1000",
+    geo_targets=["2840"],
+):
     """
     Fetch keyword ideas from Google Ads API.
     language_code: default is English (1000)
     geo_targets: default is United States (2840)
     """
     service = client.get_service("KeywordPlanIdeaService")
+    google_ads_service = client.get_service("GoogleAdsService")
 
     request = GenerateKeywordIdeasRequest(
         customer_id=customer_id,
-        language=client.get_service("GoogleAdsService").language_constant_path(language_code),
-        geo_target_constants=[client.get_service("GoogleAdsService").geo_target_constant_path(gt) for gt in geo_targets],
+        language=google_ads_service.language_constant_path(language_code),
+        geo_target_constants=[
+            google_ads_service.geo_target_constant_path(gt) for gt in geo_targets
+        ],
         keyword_plan_network=KeywordPlanNetwork.GOOGLE_SEARCH,
-        keyword_and_url_seed=KeywordAndUrlSeed(
-            url=page_url,
-            keywords=seed_keywords
-        )
+        keyword_and_url_seed=KeywordAndUrlSeed(url=page_url, keywords=seed_keywords),
     )
 
     print("\n📊 Keyword Ideas:")
@@ -46,7 +55,9 @@ def fetch_keyword_ideas(client, customer_id, page_url, seed_keywords, language_c
 
 
 if __name__ == "__main__":
-    google_ads_config_path = "google-ads.yaml"  # Make sure this YAML file is set up properly
+    google_ads_config_path = (
+        "google-ads.yaml"  # Make sure this YAML file is set up properly
+    )
     client = GoogleAdsClient.load_from_storage(google_ads_config_path)
 
     customer_id = "INSERT_YOUR_CUSTOMER_ID"
